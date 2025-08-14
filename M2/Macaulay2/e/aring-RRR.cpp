454fc1091e2ded3e17501871f04e9cdb19caabfa
Doug Torrance <dtorrance@piedmont.edu>
2025-08-14 11:46:54 -0400

    Fix multiple definitions of gmp_* tostring functions

--- M2/Macaulay2/e/aring-RRR.cpp
+++ M2/Macaulay2/e/aring-RRR.cpp
@@ -10,7 +10,7 @@ void ARingRRR::elem_text_out(buffer &o,
                              bool p_parens) const
 {
   mpfr_ptr a = &const_cast<ElementType &>(ap);
-  M2_string s = (*gmp_tostringRRpointer)(a);
+  M2_string s = (*tostringRRpointer)(a);
   bool prepend_plus = p_plus && (s->array[0] != '-');
   bool strip_last =
       !p_one && ((s->len == 1 && s->array[0] == '1') ||
